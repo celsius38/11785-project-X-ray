ca5375515707f845abb1699dace60ca1a66f9579
celsius38 <sx38@cornell.edu>
2018-11-10 00:21:14 -0500

    preprocess finished

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,9 @@ import cv2
 import os.path
 from tqdm import tqdm
 
+IMG_PATHs =["data/images" + str(i) for i in range(1,13)] 
+MAX_SIZE = 10000
+
 def get_id_label_list(): 
     """ @ Return: the entire image_id - label list with the same sequential order""" 
     data_entry = pd.read_csv("data/Data_Entry_2017.csv") 
@@ -14,29 +17,41 @@ def get_id_label_list():
     label_list = mlb.fit_transform(labels)
     return id_list, label_list
 
-IMG_PATHs =["data/images" + str(i) for i in range(1,13)]
-IMBALANCE_CUTOFF = 0.5
+def train_val_split():
+    """Split the 'data/train_val_list.txt` into two lists: train_list.npy and val_list.npy"""
 
-def save_npy():
-    """
+def save_npy(mode):
+    """ 
     for test data specified in the `test_list.txt`, generate a single npy
      file that contains the data and corresponding label in sequential order
     for train data specified in the `train_val.txt`, split into several reasonable 
      sized npy file that contains the data and corresponding label in sequential order
+    @Param: 
+        mode: "train", "val" or "test"
     @Note:
       labels are encoded using one-hot-bit encoding
       images are concatenated into N x C x W x H (N is number of images in a single set, 
       				C is 1 as black-white, W for width and H for height)
     """ 
+    def save_procedure(data, label, file_cnt):
+        """data and label are nparray, file_cnt specify the index """
+        nonlocal mode
+        data_path = "{}{}.npy".format(mode,file_cnt) 
+        label_path = "{}_label{}.npy".format(mode, file_cnt)
+        np.save(data_path, data)
+        np.save(label_path, label)
+        print("saved to {} & {}".format(data_path, label_path))
+
     id_list, label_list = get_id_label_list()
     id_idx = 0      # running ptr in the id_list
     dir_idx = 0     # ptr to the directory
     alldata, alllabel = [], []
 
+    cnt = 0; file_cnt = 0
     with open("data/test_list.txt", "r") as test_list:
         for test_id in tqdm(test_list):
             test_id = test_id.strip()
-            # loop through the index list to find the matching idx
+            # loop through the index list to find the matching idx 
             while id_list[id_idx] != test_id : 
                 id_idx += 1   
             path = os.path.join(IMG_PATHs[dir_idx], test_id)
@@ -49,12 +64,23 @@ def save_npy():
             label = label_list[id_idx] 
             alldata.append(data)
             alllabel.append(label)
-    print("finish reading, now saving to npy...")
-    alldata = np.array(alldata)
-    alllabel = np.array(alllabel)
-    np.save("test.npy", alldata)
-    np.save("test_label.npy", alllabel)
-    print("Done!")
+            id_idx += 1 
+            cnt += 1
+
+            if(cnt == MAX_SIZE):
+                print("reaching limit, now saving to npy...")
+                alldata = np.array(alldata)
+                alllabel = np.array(alllabel)
+                save_procedure(alldata, alllabel, file_cnt)
+                cnt = 0
+                file_cnt += 1 
+                alldata = []
+                alllabel = []
+        print("finalize, now saving the rest {} to npy...".format(len(alldata)))
+        alldata = np.array(alldata)
+        alllabel = np.array(alllabel)
+        save_procedure(alldata, alllabel, file_cnt)
+     
 
              
 
