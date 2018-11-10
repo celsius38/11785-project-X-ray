8945d8a56714dae34437555a7de3b3725f8c853c
celsius38 <sx38@cornell.edu>
2018-11-09 23:16:32 -0500

    preprocess updated

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,23 @@
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
-import numpy as np
 import cv2
-import matplotlib.pyplot as plt
-import glob
+import os.path
+from tqdm import tqdm
 
-def get_label_list():
-    data_entry = pd.read_csv("data/Data_Entry_2017.csv")
+def get_id_label_list(): 
+    """ @ Return: the entire image_id - label list with the same sequential order""" 
+    data_entry = pd.read_csv("data/Data_Entry_2017.csv") 
+    id_list = data_entry[["Image Index"]].values.flatten().tolist()
     labels = data_entry.apply(lambda x: x["Finding Labels"].split("|"), axis=1)
-    mlb = MultiLabelBinarizer() 
+    mlb = MultiLabelBinarizer()  
     label_list = mlb.fit_transform(labels)
-    # label_list_df = pd.DataFrame(data=label_list, columns=mlb.classes_)
-    return label_list
+    return id_list, label_list
 
-  # filename = [zip file names]
+IMG_PATHs =["data/images" + str(i) for i in range(1,13)]
+IMBALANCE_CUTOFF = 0.5
 
-IMG_PATHs =["data/images1/","data/images2/","data/images3/","data/images4/","data/images5/"]
-start_index = 0
-imbalance_cutoff = 0.5
-
-def data_preprocess(IMG_PATH,label_list):
+def save_npy():
     """
     for test data specified in the `test_list.txt`, generate a single npy
      file that contains the data and corresponding label in sequential order
@@ -29,33 +27,44 @@ def data_preprocess(IMG_PATH,label_list):
       labels are encoded using one-hot-bit encoding
       images are concatenated into N x C x W x H (N is number of images in a single set, 
       				C is 1 as black-white, W for width and H for height)
-    """
-    # TODO:
+    """ 
+    id_list, label_list = get_id_label_list()
+    id_idx = 0      # running ptr in the id_list
+    dir_idx = 0     # ptr to the directory
+    alldata, alllabel = [], []
 
-    # get the length
-    global start_index, imbalance_cutoff
+    with open("data/test_list.txt", "r") as test_list:
+        for test_id in tqdm(test_list):
+            test_id = test_id.strip()
+            # loop through the index list to find the matching idx
+            while id_list[id_idx] != test_id : 
+                id_idx += 1   
+            path = os.path.join(IMG_PATHs[dir_idx], test_id)
+            if not os.path.isfile(path):
+                dir_idx += 1
+                path = os.path.join(IMG_PATHs[dir_idx], test_id)
+                assert(os.path.isfile(path))
 
-    file_names = glob.glob(IMG_PATH + '*.png')
+            data = cv2.imread(path, 0)
+            label = label_list[id_idx] 
+            alldata.append(data)
+            alllabel.append(label)
+    print("finish reading, now saving to npy...")
+    alldata = np.array(alldata)
+    alllabel = np.array(alllabel)
+    np.save("test.npy", alldata)
+    np.save("test_label.npy", alllabel)
+    print("Done!")
 
-    num_image = len(file_names)
+             
 
-    # check label distribution
-    cur_label_list = label_list[start_index:start_index+num_image]
+        
+             
+            
+            
 
-    if (np.sum(cur_label_list,axis=0)/num_image)[10] > imbalance_cutoff:
-        label_imbalance = True
-    else:
-        label_imbalance = False
-      
-    images = []
-    for f in file_names:
-        im = cv2.imread(f, 0)
-        images.append(im)
-    image_reshape = np.expand_dims(np.array(images), axis=1)
-    # image_reshape.shape
-
-    return (image_reshape,cur_label_list),label_imbalance
-
+    
+    
 
 
 
